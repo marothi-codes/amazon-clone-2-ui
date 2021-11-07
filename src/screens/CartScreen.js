10d218e2a55c282a0c4a9da9316121a7ea0e7724
Marothi Mahlake <marothi.codes@gmail.com>
2021-11-07 22:01:40 +0200

    Cart Page completed and Remove from cart feature added.

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,24 +1,115 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import MessageBox from "../components/MessageBox";
+import { addToCart, removeFromCart } from "../redux/actions/cartActions";
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (productId) dispatch(addToCart(productId, qty));
   }, [dispatch, productId, qty]);
 
+  const handleRemoveFromCart = (productId) => {
+    // TODO: Delete action.
+    dispatch(removeFromCart(productId));
+  };
+
+  const handleCheckout = () => {
+    props.history.push(`/signin?redirect=shipping`);
+  };
+
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <p>
-        ADD TO CART: ProductID: {productId} Qty: {qty}
-      </p>
+    <div className="row top">
+      <div className="col-2">
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            The cart is empty.{" "}
+            <Link to="/">
+              Start Shopping <i className="fa fa-shopping-cart"></i>
+            </Link>
+          </MessageBox>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  <div>
+                    <img src={item.image} alt={item.name} className="small" />
+                  </div>
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  <div>
+                    <select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>R{item.price}</div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={(event) => handleRemoveFromCart(item.product)}
+                    >
+                      <i className="fa fa-trash-o"></i> Remove
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="col-1">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal (
+                {cartItems.reduce(
+                  (accumulator, currentItem) => accumulator + currentItem.qty,
+                  0
+                )}{" "}
+                items) : R
+                {cartItems.reduce(
+                  (accumulator, currentItem) =>
+                    accumulator + currentItem.price * currentItem.qty,
+                  0
+                )}
+              </h2>
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={handleCheckout}
+                className="primary block"
+                disabled={cartItems.length === 0}
+              >
+                Proceed to Checkout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
